3628e08fe7820621af46cfc9fee2ae2156086193
saliao <saliao@ucsc.edu>
2024-01-23 04:43:53 -0800

    Added results

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -41,27 +41,88 @@ function setup() {
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+    noLoop();
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    var randomx = random((0,255));
+  createCanvas(400, 400);
+  background(0);
+  blendMode(ADD);
+  drawImage();
+  
+  
+  // moon
+  fill(255);
+  ellipse(200, 320, 150, 150);
+  
+  
+  drawingContext.shadowColor = color(randomx, randomx, randomx);
+  drawingContext.shadowBlur = 12;
+  stroke(0)
+  //fill(255)
+  fill(0);
+  // Head
+  ellipse(200, 55, 20, 25);
+  
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  // Body
+  
+  //neck
+  strokeWeight(12);
+  line(200, 60, 200, 140);
+  
+  //Torso
+  strokeWeight(10);
+  beginShape() 
+    vertex(200, 80);
+    vertex(190,90)
+    vertex(225, 90)   
+  endShape() 
+  rect(195, 90, 10, 50);
+  
+  //Feet
+  //Hands
+  // shoulders
+  line(200, 80, 180, 90);
+  
+  line(200, 80, 225, 90);
+  
+  // Arms
+  line(180, 90, 165, 130);
+  line(225, 90, 245, 130);
+  
+  //forearms
+  line(165, 130, 165, 180);
+  line(245, 130, 240, 180);
+
+  // Left Leg
+  line(195, 140, 160, 300);
+
+  // Right Leg
+  line(205, 140, 240, 300);
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
 }
+function drawImage() {	
+  
+	drawingContext.shadowBlur = 32;
+	drawingContext.shadowColor = color(200, 220, 255);
+	for(var i = 0; i < pointsNum; i++) {
+		let r = random(2 ,8);
+		noStroke();
+		fill(200, 220, 255);
+		var x=random(width);
+		var y=random(height);
+		for(var j = 0; j < 6; j++)ellipse(x, y, r, r);
+	}
+}
+function mouseMoved() {
+    clear();
+    redraw();
+    
+  }
