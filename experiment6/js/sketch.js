f7e3217db2819835e558915a3fca83f154ed930a
saliao <saliao@ucsc.edu>
2024-02-21 01:29:09 -0800

    Update sketch.js

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -173,7 +173,7 @@ function setup() {
   createCanvas(800, 800);
   textAlign(CENTER, CENTER);
   var button = createButton('insult me');
-  button.position(width/2-50, 1600);
+  button.position(width/2, 1400);
 
   // Use the button to change the background color.
   button.mousePressed(() => {
